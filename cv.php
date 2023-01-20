31ae583567e2dfbaa5f49548bb186184d5ed94e0
ridwan0805 <123130164+ridwan0805@users.noreply.github.com>
2023-01-20 11:14:23 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cv.css">
+    <title>CV Saya</title>
+</head>
+<nav>
+    <div class="wrapper">
+        <div class="logo"><a><img src="image/profil.jpeg" style="width: 150px;border-radius:20%;border:3px solid #7d5e4a;"></a></div>
+        <div class="profil">
+            <h2>Muhammad Ridwan Maulana</h2>
+            <p>Art Designer</p>
+        </div>
+    </div>
+</nav>
+
+<body>
+    <section>
+        <div class="profil2">
+            <p style="font-size:25px;margin-bottom:15px;background-color:brown;color:white;padding:10px">Kontak</p>
+            <section><img src="image/tt.png" style="width: 30px;margin-right:20px"><p> 0895-3313-72540</p></section>
+            <section><img src="image/ig.png" style="width: 30px;margin-right:20px"><p> mln.rdwn87@gmail.com</p></section>
+            <section><img src="image/yt.png" style="width: 30px;margin-right:20px"><p> 0057921719@smkn4padalarang.sch.id</p></section>
+        </div>
+        <div class="profil2">
+            <p style="font-size:25px;margin-bottom:15px;background-color:brown;color:white;padding:10px">Tentang Saya</p>
+            <p>Nama saya Muhammad Ridwan Maulana. Saya lahir pada tanggal 8 Mei 2005 di Bandung. Saya sering membuat desain di media sosial. Saya juga berpengalaman membuat website-website sederhana.</p>
+        </div>
+    </section>
+    <section>
+        <div class="profil2">
+            <p style="font-size:25px;margin-bottom:20px;background-color:brown;color:white;padding:10px">Keahlian</p>
+            <section>
+                <p style="text-align:center;margin-bottom:10px;margin-right:10px;background-color:rgb(88, 170, 97);padding:15px;border-radius:15px">Ilustrator</p>
+                <p style="text-align:center;margin-bottom:10px;margin-right:10px;background-color:rgb(88, 170, 97);padding:15px;border-radius:15px">Microsoft Word</p>
+            </section>
+            <section>
+                <p style="text-align:center;margin-bottom:10px;margin-right:10px;background-color:rgb(88, 170, 97);padding:15px;border-radius:15px">Microsoft Excel</p>
+                <p style="text-align:center;margin-bottom:10px;margin-right:10px;background-color:rgb(88, 170, 97);padding:15px;border-radius:15px">PHP</p>
+            </section>
+        </div>
+        <div class="profil2">
+            <p style="font-size:25px;margin-bottom:15px;background-color:brown;color:white;padding:10px">Pengalaman Kerja</p>
+            <p>Saya pernah melaksanakan PKL selama tiga bulan di IKIP SILWANGI di bagian fakultas pendidikan bahasa sebagai admin fakultas</p>
+        </div>
+    </section>
+    <section>
+        <div class="profil2" style="background-color: white;padding:25px;">
+            <p style="font-size:25px;margin-bottom:15px;background-color:brown;color:white;padding:10px;border-radius:5px">Riwayat Pendidikan</p>
+            <div style="background-color: #c5a38d;padding:10px;border-radius:5px">
+            <p style="font-size:18px">2011-2012</p>
+            <p style="font-weight:bold ;">PAUD KASIH IBU</p>
+            </div>
+            <div style="background-color: #c5a38d;padding:10px;border-radius:5px;margin:10px auto">
+            <p>2012-2017</p>
+            <p>SDN 2 KERTAMULYA</p>
+            </div>
+            <div style="background-color: #c5a38d;padding:10px;border-radius:5px;margin:10px auto">
+            <p>2017-2020</p>
+            <p>SMP KRIDA UTAMA PADALARANG</p>
+            </div>
+            <div style="background-color: #c5a38d;padding:10px;border-radius:5px;margin:10px auto">
+            <p>2020-2023</p>
+            <p>SMK NEGERI 4 PADALARANG</p>
+            </div>
+        </div>
+        <div class="profil2">
+        </div>
+    </section>
+</body>
+
+</html>
